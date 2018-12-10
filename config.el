d02a23ef9272fce5e2a75b994ef1e9f9afdf7e59
indraniel <indraniel@gmail.com>
2018-12-10 09:02:59 -0600

    + add some additional configs for clojure & scheme
      - i don't understand why these aren't taking effect yet...

--- config.el
+++ config.el
@@ -1,3 +1,13 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 ;; Place your private configuration here
+
+(after! clojure-mode
+  (rainbow-delimiters-mode)
+  (paredit-mode)
+  (evil-cleverparens-mode))
+
+(after! scheme-mode
+  (rainbow-delimiters-mode)
+  (paredit-mode)
+  (evil-cleverparens-mode))
